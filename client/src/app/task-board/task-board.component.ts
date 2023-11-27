fa1a7492af8fe205c6e7e28aa376e8e0694f5c2d
drespana <drespana@google.com>
2023-11-27 11:55:16 -0600

    added task board where user will be able to add weekly or monthly tasks and reminders

---
+++ client/src/app/task-board/task-board.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-board',
+  templateUrl: './task-board.component.html',
+  styleUrls: ['./task-board.component.css']
+})
+export class TaskBoardComponent {
+
+  addingReminder:boolean=false;
+  addingTask:boolean=false;
+  weeklyCalendar:boolean=true;
+  monthlyCalendar:boolean=false;
+
+  toggleCalendar(){
+    this.weeklyCalendar = !this.weeklyCalendar
+    this.monthlyCalendar = !this.monthlyCalendar
+  }
+
+}
